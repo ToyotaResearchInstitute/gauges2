8652641d6c5202dc87ebafcd1d9d2a62dd27202b
Alejandro Hernández Cordero <ahcorde@gmail.com>
2024-02-20 10:14:40 +0100

    0.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ package_name = 'rqt_gauges'
 
 setup(
     name=package_name,
-    version='0.0.1',
+    version='0.0.2',
     packages=[package_name],
     package_dir={'': 'src'},
     data_files=[
